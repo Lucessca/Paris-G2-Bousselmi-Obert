3e2764fb0b100ced512f324ecbd118912120fef4
Lucessca <luc.obert@essca.eu>
2023-02-27 15:40:16 +0100

    Update python_project_obert.py

--- python_project_obert.py
+++ python_project_obert.py
@@ -14,3 +14,18 @@ e="hello my team - I am Benjamin"
 print(a,b,c,d,e)
 
 #modif
+
+import seaborn as sns
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as 
+
+#HEATMAP, Exercise 1.-------------------------------------------------------------
+# Create a dataset
+df = pd.DataFrame(np.random.random((5,5)), columns=["a","b","c","d","e"])
+# numpy.random.random() is one of the function for doing random sampling in numpy. 
+#It returns an array of specified shape and fills it with random floats in the half-open interval [0.0, 1.0).
+
+
+# Default heatmap            ------- plot first heatmap
+p1 = sns.heatmap(df)
