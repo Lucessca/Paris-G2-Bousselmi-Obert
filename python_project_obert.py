cb95db2f061b5936808c0c6d93a731f4efb0cb12
Martibou <126480125+Martibou@users.noreply.github.com>
2023-02-27 15:42:26 +0100

    Update python_project_obert.py

--- python_project_obert.py
+++ python_project_obert.py
@@ -14,3 +14,15 @@ d="hello my team - I am Martin"
 print(a,b,c,d,)
 
 #modif
+
+#Exercise 5--------------------------------------------------------------------------
+
+import matplotlib.pyplot as plt
+
+column_labels = list('ABC')
+row_labels = list('WXYZ')
+data = np.array([[1, 2, 3], [0, 3, 2], [1, 2, 3], [4, 3, 2]]) 
+fig, axis = plt.subplots() 
+heatmap = axis.pcolor(data, cmap=plt.cm.Greens)
+plt.savefig('test.png')
+plt.show() 
