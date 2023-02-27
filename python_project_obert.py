47f3b7e8b867d6654d4721353d62ee2d8373a556
Matheo501 <matheo.langlais@essca.eu>
2023-02-27 15:23:01 +0100

    Update python_project_obert.py

--- python_project_obert.py
+++ python_project_obert.py
@@ -6,9 +6,11 @@ Created on Mon Feb 27 14:32:52 2023
 @author: lucobert
 """
 
-print("hello my team - I am Luc")
-print("hello my team - I am Matheo")
-print("hello my team - I am Quentin")
-print("hello my team - I am Martin")
+a="hello my team - I am Luc"
+b="hello my team - I am Matheo"
+c="hello my team - I am Quentin"
+d="hello my team - I am Martin"
+
+print(a,b,c,d,)
 
 #modif
