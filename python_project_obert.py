eafc69a5752b70cc83c4326d783d7a9d8bbe04f8
Lucessca <126480107+Lucessca@users.noreply.github.com>
2023-03-21 16:31:11 +0100

    Update python_project_obert.py

--- python_project_obert.py
+++ python_project_obert.py
@@ -6,26 +6,14 @@ Created on Mon Feb 27 14:32:52 2023
 @author: lucobert
 """
 
-a="hello my team - I am Luc"
-b="hello my team - I am Matheo"
-c="hello my team - I am Quentin"
-d="hello my team - I am Martin"
-e="hello my team - I am Benjamin"
-print(a,b,c,d,e)
+# Sock price import
 
-#modif
+import pandas_datareader.data as pdr
+import yfinance as yf
+yf.pdr_override()
+from datetime import datetime
 
-import seaborn as sns
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as 
-
-#HEATMAP, Exercise 1.-------------------------------------------------------------
-# Create a dataset
-df = pd.DataFrame(np.random.random((5,5)), columns=["a","b","c","d","e"])
-# numpy.random.random() is one of the function for doing random sampling in numpy. 
-#It returns an array of specified shape and fills it with random floats in the half-open interval [0.0, 1.0).
-
-
-# Default heatmap            ------- plot first heatmap
-p1 = sns.heatmap(df)
+WMT = pdr.get_data_yahoo('WMT',
+                          start=datetime(2013, 1, 1),  ##(yyyy, dd, mm)
+                          end=datetime(2023, 21, 3))     ##(yyyy, dd, mm)
+print(WMT)
