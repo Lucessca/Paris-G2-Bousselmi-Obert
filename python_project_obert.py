abe9b65d7af848ab55ed9ee7b4a4b6c36cbb15e9
Lucessca <126480107+Lucessca@users.noreply.github.com>
2023-03-30 11:40:59 +0200

    Update python_project_obert.py

--- python_project_obert.py
+++ python_project_obert.py
@@ -229,6 +229,103 @@ def mygput(s, k, r, b, sigma, t):
 
 # print results
 
+
+
+
+
+
+
+
+
+#SMA Strategy: creating signals - Exercise 8
+#-----------------------------------------------------------------------------
+# Initialize the short and long windows
+short_window = 40
+long_window = 100
+
+# Initialize the `signals` DataFrame with the `signal` column
+signals = pd.DataFrame(index=aapl.index)
+signals['signal'] = 0.0
+
+# Create short simple moving average over the short window
+signals['short_mavg'] = aapl['Close'].rolling(window=short_window, min_periods=1, center=False).mean()
+
+# Create long simple moving average over the long window
+signals['long_mavg'] = aapl['Close'].rolling(window=long_window, min_periods=1, center=False).mean()
+
+# Create signals
+signals['signal'][short_window:] = np.where(signals['short_mavg'][short_window:]
+                                            > signals['long_mavg'][short_window:], 1.0, 0.0)  
+
+# Generate trading orders
+signals['positions'] = signals['signal'].diff()
+
+# Print `signals`
+print(signals)
+
+# Plot your signals - Exercise 9
+#-----------------------------------------------------------------------------
+# Import `pyplot` module as `plt`
+import matplotlib.pyplot as plt
+
+# Initialize the plot figure
+fig = plt.figure()
+
+# Add a subplot and label for y-axis
+ax1 = fig.add_subplot(111,  ylabel='Price in $')
+
+# Plot the closing price
+aapl['Close'].plot(ax=ax1, color='r', lw=2.)
+
+# Plot the short and long moving averages
+signals[['short_mavg', 'long_mavg']].plot(ax=ax1, lw=2.)
+
+# Plot the buy signals (magenta)
+ax1.plot(signals.loc[signals.positions == 1.0].index,
+         signals.short_mavg[signals.positions == 1.0],
+         '^', markersize=10, color='m')
+         
+# Plot the sell signals (black)
+ax1.plot(signals.loc[signals.positions == -1.0].index,
+         signals.short_mavg[signals.positions == -1.0],
+         'v', markersize=10, color='k')
+         
+# Show the plot
+plt.show()
+
+# Backtesting a trading strategy - Exercise 10
+#-----------------------------------------------------------------------------
+# Set the initial capital
+initial_capital= float(100000.0)
+
+# Create a DataFrame `positions`
+positions = pd.DataFrame(index=signals.index).fillna(0.0)
+
+# Buy a 100 shares
+positions['AAPL'] = 100*signals['signal']  
+ 
+# Initialize the portfolio with value owned  
+portfolio = positions.multiply(aapl['Adj Close'], axis=0)
+
+# Store the difference in shares owned
+pos_diff = positions.diff()
+
+# Add `holdings` to portfolio
+portfolio['holdings'] = (positions.multiply(aapl['Adj Close'], axis=0)).sum(axis=1)
+
+# Add `cash` to portfolio
+portfolio['cash'] = initial_capital - (pos_diff.multiply(aapl['Adj Close'], axis=0)).sum(axis=1).cumsum()  
+
+# Add `total` to portfolio
+portfolio['total'] = portfolio['cash'] + portfolio['holdings']
+
+# Add `returns` to portfolio
+portfolio['returns'] = portfolio['total'].pct_change()
+
+# Print the first lines of `portfolio`
+print(portfolio.head())
+
+
 print('Result is - Generalized formula of B&S - call:')
 print(mygcall(s, k, r, b, sigma, t))
 print('Result is - Generalized formula of B&S - put:')
