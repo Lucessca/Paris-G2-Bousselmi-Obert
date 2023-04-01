494139d4235b3e5407f846b37fbc54aaa5c78c62
Matheo501 <126480045+Matheo501@users.noreply.github.com>
2023-04-01 18:12:22 +0200

    Update python_project_obert.py

--- python_project_obert.py
+++ python_project_obert.py
@@ -1,431 +1,197 @@
 # -*- coding: utf-8 -*-
 """
-Created on Thu Mar 30 10:28:27 2023
+Created on Sat Apr  1 12:12:55 2023
 
 @author: mathe
 """
 
-
+#----------------------------------------------------------------------------------------------------------------#
+#    FINAL VERSION        PYTHON PROJECT
+#----------------------------------------------------------------------------------------------------------------#
 import pandas_datareader.data as pdr
 import yfinance as yf
 import matplotlib.pyplot as plt
-import scipy.stats as si
 import numpy as np
+import scipy.stats as si
 import pandas as pd
 yf.pdr_override()
 from datetime import datetime
 from datetime import date
 from datetime import timedelta
 
-WMT = pdr.get_data_yahoo('WMT',
-                          start=datetime(2013, 1, 1),  ##(yyyy, dd, mm)
-                          end=datetime(2023, 1, 3))     ##(yyyy, dd, mm)
-
-# Isolate the adjusted closing prices
-adj_close_px = WMT[['Adj Close']]
-# Calculate the moving average for aapl
-moving_avg = adj_close_px.rolling(window=40).mean()
-# Short moving window rolling mean
-WMT['42'] = adj_close_px.rolling(window=40).mean()
-# Long moving window rolling mean
-WMT['252'] = adj_close_px.rolling(window=252).mean()
-# Plot the adjusted closing price, the short and long windows of rolling means
-WMT[['Adj Close', '42', '252']].plot()
- 
-# Show plot
-plt.show()
-
-print(WMT)
-
-
-KO = pdr.get_data_yahoo('KO',
-                          start=datetime(2013, 1, 1),  ##(yyyy, dd, mm)
-                          end=datetime(2023, 1, 3))     ##(yyyy, dd, mm)
-# Isolate the adjusted closing prices
-adj_close_px = KO[['Adj Close']]
-# Calculate the moving average for aapl
-moving_avg = adj_close_px.rolling(window=40).mean()
-# Short moving window rolling mean
-KO['42'] = adj_close_px.rolling(window=40).mean()
-# Long moving window rolling mean
-KO['252'] = adj_close_px.rolling(window=252).mean()
-# Plot the adjusted closing price, the short and long windows of rolling means
-KO[['Adj Close', '42', '252']].plot()
- 
-# Show plot
-plt.show()
-
-print(KO)
-
-
-SBUX = pdr.get_data_yahoo('SBUX',
-                          start=datetime(2013, 1, 1),  ##(yyyy, dd, mm)
-                          end=datetime(2023, 1, 3))     ##(yyyy, dd, mm)
-# Isolate the adjusted closing prices
-adj_close_px = SBUX[['Adj Close']]
-# Calculate the moving average for aapl
-moving_avg = adj_close_px.rolling(window=40).mean()
-# Short moving window rolling mean
-SBUX['42'] = adj_close_px.rolling(window=40).mean()
-# Long moving window rolling mean
-SBUX['252'] = adj_close_px.rolling(window=252).mean()
-# Plot the adjusted closing price, the short and long windows of rolling means
-SBUX[['Adj Close', '42', '252']].plot()
- 
-# Show plot
-plt.show()
-
-print(SBUX)
-
-
-MCD = pdr.get_data_yahoo('MCD',
-                          start=datetime(2013, 1, 1),  ##(yyyy, dd, mm)
-                          end=datetime(2023, 1, 3))     ##(yyyy, dd, mm)
-# Isolate the adjusted closing prices
-adj_close_px = MCD[['Adj Close']]
-# Calculate the moving average for aapl
-moving_avg = adj_close_px.rolling(window=40).mean()
-# Short moving window rolling mean
-MCD['42'] = adj_close_px.rolling(window=40).mean()
-# Long moving window rolling mean
-MCD['252'] = adj_close_px.rolling(window=252).mean()
-# Plot the adjusted closing price, the short and long windows of rolling means
-MCD[['Adj Close', '42', '252']].plot()
- 
-# Show plot
-plt.show()
-
-print(MCD)
-
-
-
-
-# Isolate the adjusted closing prices
-adj_close_px = MCD[['Adj Close']]
-# Calculate the moving average for aapl
-moving_avg = adj_close_px.rolling(window=40).mean()
-# Short moving window rolling mean
-MCD['42'] = adj_close_px.rolling(window=40).mean()
-# Long moving window rolling mean
-MCD['252'] = adj_close_px.rolling(window=252).mean()
-# Plot the adjusted closing price, the short and long windows of rolling means
-MCD[['Adj Close', '42', '252']].plot()
- 
-# Show plot
-plt.show()
- 
- 
-#aapl['42max'] = adj_close_px.rolling(window=40).max()
-#aapl['42min'] = adj_close_px.rolling(window=40).min()
-#aapl[['Adj Close', '42', '252', '42max', '42min']].plot()
-#plt.show()
-
-MCD = pdr.get_data_yahoo('MCD',
-                          start=datetime(2013, 1, 1),  ##(yyyy, dd, mm)
-                          end=datetime(2023, 1, 3))     ##(yyyy, dd, mm)
-print(MCD)
-
-
-
-#   step 5 
-# s: spot price
-# k: strike price
-# r: risk-free
-# b : cost of carry rate
-# sigma: volatility of uderlying asset
-# t: time to maturity
-#
-#--------------------- possible values of b
-# b = r         # european stock oprions with no-dividend
-# b = r-q       # european stock options with dividends
-# b = 0         # european futures options
-# b = r - rj    # european currency options
- 
-# ----------------------------------------------------------------------------
- 
-# Generalized BM: call (1 and 2)
-
-# BS define last value of the last row
-
-last_row_WMT = WMT.tail(1) 
-last_value_WMT = last_row_WMT.iloc[0][-4] 
-
-
-# BS volatility calculation
-
-WMT = yf.Ticker("WMT") 
-df = WMT.history( start=datetime(2013, 1, 1),end=datetime(2023, 1, 3))
-#        calculate daily returns 
-df['daily_return'] = df['Close'].pct_change() 
-#        calculate annualized volatility 
-volatility = df['daily_return'].std() * (252**0.5) 
-
-
-# BS risk free rate calculation in percentage (average of the last 7 days)
-
-rf = pdr.get_data_yahoo('^TNX', start=date.today()- timedelta(days = 8), end=date.today()- timedelta(days = 1))
-
-adj_close_px = rf[['Adj Close']]
-avgrf = adj_close_px.mean()
-
-last_row_rf = rf.tail(1) 
-last_value_rf = last_row_rf.iloc[0][-0] / 100
-
-
-
-
-
-
-
-
-
-
-
-#si achete (buy) de l action alors on veut couvrir une perte lie a un cours de bourse qui 
-#baisserait donc on souhaite limiter notre risque maximum a 2.5% donc on veut acheter un option qui nous donne le
-#droit de vendre a -2.5% du cours de bourse actuel
-
-#si long dans l autre sens
-
-#Programmer un IF ... pour determiner la valeur de k selon le sens dans lequel on prend notre option sur la 
-#base du croisement des MMA (strategie de trading)
-
-
-
-
-
-
-
-
-
-
-
-# BS variable definition
- 
-s = last_value_WMT
-k = last_value_WMT*0.9
-r = last_value_rf
-t = 3 / 12 #arbitrarly choosen as we decide to cover our positon for maximum 3 months
-sigma = volatility
-b = r #european stock oprions with no-dividend
-
-
-# BS call defintion
-
-def mygcall(s, k, r, b, sigma, t):
- 
-    d1 = (np.log(s / k) + t * (b + sigma ** 2 / 2)) / ( sigma * np.sqrt(t))
-   
-    d2 = d1 - sigma * np.sqrt(t)
-   
-    call = s * np.exp( t * (b-r) ) * si.norm.cdf(d1) - k * np.exp(-r * t) * si.norm.cdf(d2)
-   
-        
-    return call
-
-
-# BS put defintion
-
-def mygput(s, k, r, b, sigma, t):
- 
-    d1 = (np.log(s / k) + t * (b + sigma ** 2 / 2)) / ( sigma * np.sqrt(t))
-   
-    d2 = d1 - sigma * np.sqrt(t)
-   
-    put = k * np.exp(-r * t) * si.norm.cdf(-d2) - s * np.exp( t * (b-r)) * si.norm.cdf(-d1)
-   
-    return put
-
-# print results
-
-print('Result is - Generalized formula of B&S - call:')
-print(mygcall(s, k, r, b, sigma, t))
-print('Result is - Generalized formula of B&S - put:')
-print(mygput(s, k, r, b, sigma, t))
-
-
-
-# Isolate the `Adj Close` values and transform the DataFrame
-daily_close_px = WMT[['Adj Close']].reset_index().pivot('Date', 'Ticker', 'Adj Close')
-
-
-
-
-
-
-
-
-
-#SMA Strategy: creating signals - Exercise 8
-#-----------------------------------------------------------------------------
-# Initialize the short and long windows
-short_window = 40
-long_window = 100 #--> MMA
-
-# Initialize the `signals` DataFrame with the `signal` column
-signals = pd.DataFrame(index=WMT.index)
-signals['signal'] = 0.0
-
-# Create short simple moving average over the short window
-signals['short_mavg'] = WMT['Close'].rolling(window=short_window, min_periods=1, center=False).mean()
-
-# Create long simple moving average over the long window
-signals['long_mavg'] = WMT['Close'].rolling(window=long_window, min_periods=1, center=False).mean()
-
-# Create signals
-signals['signal'][short_window:] = np.where(signals['short_mavg'][short_window:]
-                                            > signals['long_mavg'][short_window:], 1.0, 0.0)  
-
-# Generate trading orders
-signals['positions'] = signals['signal'].diff()
-
-# Print `signals`
-print(signals)
-
-
-
-
-
-
-
-
-
-
-
-# Plot your signals - Exercise 9
-#-----------------------------------------------------------------------------
-# Import `pyplot` module as `plt`
-import matplotlib.pyplot as plt
-
-# Initialize the plot figure
-fig = plt.figure()
-
-# Add a subplot and label for y-axis
-ax1 = fig.add_subplot(111,  ylabel='Price in $')
-
-# Plot the closing price
-WMT['Close'].plot(ax=ax1, color='r', lw=2.)
-
-# Plot the short and long moving averages
-signals[['short_mavg', 'long_mavg']].plot(ax=ax1, lw=2.)
-
-# Plot the buy signals (magenta)
-ax1.plot(signals.loc[signals.positions == 1.0].index,
-         signals.short_mavg[signals.positions == 1.0],
-         '^', markersize=10, color='m')
-         
-# Plot the sell signals (black)
-ax1.plot(signals.loc[signals.positions == -1.0].index,
-         signals.short_mavg[signals.positions == -1.0],
-         'v', markersize=10, color='k')
-         
-# Show the plot
-plt.show()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-#----------------------------------------------------------------------------------------------------------------#
-#    FINAL VERSION        PYTHON PROJECT
-#----------------------------------------------------------------------------------------------------------------#
-
-
-
-
-# Define list of tickers
+# Define variables
 tickers = ['WMT', 'KO', 'SBUX', 'MCD']
-
-
-# Loop through tickers and retrieve data
-for ticker in tickers:
-    # Retrieve data
-    data = pdr.get_data_yahoo(ticker, start=datetime(2013, 1, 1), end=datetime(2023, 1, 3))
-        
-    # Calculate rolling means
-    adj_close_px = data[['Adj Close']]
-    data['42'] = adj_close_px.rolling(window=40).mean()
-    data['252'] = adj_close_px.rolling(window=252).mean()
-    
-    # Plot data
-    data[['Adj Close', '42', '252']].plot()
-    plt.title(ticker)
-    plt.show()
-    
-    # Print data
-    print(data)
-
-
-    # --- BACKTESTING --- #
-    # Create the signal column in the data table
-    data['signal'] = 0.0
-    
-    # Generate the signal
-    data['signal'][short_window:]=np.where(data['42'][short_window:]>data['252'][short_window:],1.0,0.0)
-    
-    # Inform on position
-    data['positions'] = data['signal'].diff()
-    
-    # Print signals
-    print(data['positions'])
-
-
-
-
-
-# Backtesting a trading strategy - Exercise 10
-#-----------------------------------------------------------------------------
-# Set the initial capital
+short_window = 7
+long_window = 21
 initial_capital= float(100000.0)
 
-# Create a DataFrame `positions`
-positions = pd.DataFrame(index=data['signal'].index).fillna(0.0)
+# Loop through tickers and retrieve data
+for tick in tickers:
+    # Loop through tickers and retrieve data
 
-# Buy a 100 shares
-positions['WMT'] = 100*data['signal']  
+    # ----- Retrieve data -----
+
+    data = pdr.get_data_yahoo(tick, start=datetime(2000, 1, 1), end=datetime(2023, 1, 3))
+
+    # ----- Signals creation -----
+
+    # Initialize the `signals` DataFrame with the `signal` column
+    signals = pd.DataFrame(index=data.index)
+    signals['signal'] = 0.0
+
+    # Create moving averages
+    signals['short_mavg'] = data['Adj Close'].rolling(window=short_window, min_periods=1, center=False).mean()
+    signals['long_mavg'] = data['Adj Close'].rolling(window=long_window, min_periods=1, center=False).mean()
+
+    # Create signals
+    signals['signal'][short_window:] = np.where(signals['short_mavg'][short_window:] > signals['long_mavg'][short_window:], 1.0, 0.0)
+
+    # Generate trading orders
+    signals['positions'] = signals['signal'].diff()
+
+    # ----- Ploting the signals and moving averages -----
+
+    # Initialize the plot figure
+    fig = plt.figure()
+
+    # Add a subplot and label for y-axis
+    ax1 = fig.add_subplot(111,  ylabel='Price in $')
+
+    # Plot the closing price
+    data['Adj Close'].plot(ax=ax1, color='k', lw=1.)
+
+    # Plot the short and long moving averages
+    signals[['short_mavg']].plot(ax=ax1, color='m', lw=1.)
+    signals[['long_mavg']].plot(ax=ax1, color='b', lw=1.)
+
+    # Plot the buy signals (magenta)
+    ax1.plot(signals.loc[signals.positions == 1.0].index, signals.short_mavg[signals.positions == 1.0], '^', markersize=4, color='g')
+    ax1.plot(signals.loc[signals.positions == -1.0].index, signals.short_mavg[signals.positions == -1.0], 'v', markersize=4, color='r')
+             
+    # Show the plot
+    plt.show()
+
+    # ----- Backtesting the strategy -----
+
+    # Create a DataFrame `positions`
+    positions = pd.DataFrame(index=signals.index).fillna(0.0)
+    Posize = pd.DataFrame(index=signals.index).fillna(0.0)
+    portfolio = pd.DataFrame(index=signals.index).fillna(0.0)
+
+    # Merge adjusted close from data to signals data frame
+    Merged_data = pd.merge(data, signals, left_index=True, right_index=True)
+
+    # Stop loss calculation
+    SL = np.where(Merged_data['signal'] == 1.0, Merged_data['Adj Close'] * (1 - 0.2), 0.0)
+
+    # Buy a 5,000 shares
+    positions[tick] = 5000*signals['signal']  
+
+    # Initialize the portfolio with value owned
+    portfolio = pd.DataFrame(positions).multiply(Merged_data['Adj Close'], axis=0)
+
+    # Store the difference in shares owned
+    pos_diff = positions.diff()
+
+    # Add `holdings` to portfolio
+    portfolio['holdings'] = (positions.multiply(Merged_data['Adj Close'], axis=0)).sum(axis=1)
+
+    # Add `cash` to portfolio
+    portfolio['cash'] = initial_capital - (pos_diff.multiply(Merged_data['Adj Close'], axis=0)).sum(axis=1).cumsum()  
+
+    # Add `total` to portfolio
+    portfolio['total'] = portfolio['cash'] + portfolio['holdings']
+
+    # Add `returns` to portfolio
+    portfolio['returns'] = portfolio['total'].pct_change()
+
+    # Create a figure
+    fig = plt.figure()
+
+    ax1 = fig.add_subplot(111, ylabel='Portfolio value in $')
+
+    # Plot the equity curve in dollars
+    portfolio['total'].plot(ax=ax1, color='k', lw=1.)
+
+    ax1.plot(portfolio.loc[signals.positions == 1.0].index, portfolio.total[signals.positions == 1.0], '^', markersize=4, color='g')
+    ax1.plot(portfolio.loc[signals.positions == -1.0].index, portfolio.total[signals.positions == -1.0], 'v', markersize=4, color='r')
+
+    # Show the plot
+    plt.show()
+
+    # ----- Compute the benefits generated by purchaisng 1,000 shares in each stocks -----
+    # Calculate annual returns 
+    Avg_return = portfolio['returns'].mean()
+    ANNUAL =  ((Avg_return+1)**252 - 1)*100   
+        
+    # ----- Compute the last Black and Scholes (BS) option price -----
+    
+    #------------------------------------------------------------
+    
+    # Parameteres explanation
+    
+    # s: spot price (last price for each of the tickers)
+    # k: strike price (10% below the spot price: the maximum tolerated risk)
+    # r: risk-free (10-years USA)
+    # b: cost of carry rate equitvalent to risk-free rate (we consider our stocks to not deliver any dividends)
+    # sigma: volatility of the uderlying ticker
+    # t: time to maturity (set to be 3/12 of a year, after 3-monts from taking the position, if it is still loosing money the option will be executed, otherwise no)
  
-# Initialize the portfolio with value owned  
-portfolio = positions.multiply(WMT['Adj Close'], axis=0)
+    #------------------------------------------------------------
+    
 
-# Store the difference in shares owned
-pos_diff = positions.diff()
+    # Generalized BM: call (1 and 2)
+    
+    # Define last value of the last row of the dataset (regarding the concerned ticker)
+    last_row = data.tail(1) 
+    last_value = last_row.iloc[0][-4] 
 
-# Add `holdings` to portfolio
-portfolio['holdings'] = (positions.multiply(WMT['Adj Close'], axis=0)).sum(axis=1)
+    # BS volatility calculation
+    t = yf.Ticker(tick) 
+    df = t.history( start=datetime(2013, 1, 1),end=datetime(2023, 1, 3))
+    # Calculate daily returns 
+    df['daily_return'] = df['Close'].pct_change() 
+    # Calculate annualized volatility 
+    volatility = df['daily_return'].std() * (252**0.5) 
+    
+    
+    # BS risk free rate calculation in percentage (average of the last 7 days)
+    rf = pdr.get_data_yahoo('^TNX', start=date.today()- timedelta(days = 8), end=date.today()- timedelta(days = 1))
+    adj_close_px = rf[['Adj Close']]
+    avgrf = adj_close_px.mean()
+    last_row_rf = rf.tail(1) 
+    last_value_rf = last_row_rf.iloc[0][-0] / 100
 
-# Add `cash` to portfolio
-portfolio['cash'] = initial_capital - (pos_diff.multiply(WMT['Adj Close'], axis=0)).sum(axis=1).cumsum()  
+    # BS variable definition
+    s = last_value
+    k = last_value*0.9
+    r = last_value_rf
+    t = 3 / 12 
+    sigma = volatility
+    b = r
+    
+    # BS put defintion, only the put price is calculated as only long position will be taken
+    def mygput(s, k, r, b, sigma, t):
+        d1 = (np.log(s / k) + t * (b + sigma ** 2 / 2)) / ( sigma * np.sqrt(t))
+        d2 = d1 - sigma * np.sqrt(t)
+        put = k * np.exp(-r * t) * si.norm.cdf(-d2) - s * np.exp( t * (b-r)) * si.norm.cdf(-d1)
+        return put
 
-# Add `total` to portfolio
-portfolio['total'] = portfolio['cash'] + portfolio['holdings']
-
-# Add `returns` to portfolio
-portfolio['returns'] = portfolio['total'].pct_change()
-
-# Print the first lines of `portfolio`
-print(portfolio.head())
+    # ----- PRINT THE SUMMARY -----
+    print(' ')
+    print(' ')
+    print(' ')
+    print('*----------------------------------------------------------------------------------------------------------------*')
+    print('REPORT - Analysis since 2000')
+    print('*----------------------------------------------------------------------------------------------------------------*')
+    print(' ')
+    print('For ticker:')
+    print(tick)
+    print(' ')
+    print('The average annual return with 5,000 shares was:')
+    print(ANNUAL)
+    print(' ')
+    print('If you decided to invest in it today the value of one put european option, to cover your long, is:')
+    print(mygput(s, k, r, b, sigma, t))
+    print(' ')
+    print(' ')
